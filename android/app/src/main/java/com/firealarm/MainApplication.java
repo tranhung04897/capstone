c8be2aeb7354b69fa6c405cb2582ef6d2e98f41f
Hưng Trần <Hung Tran>
2018-11-09 21:33:15 +0700

    Config firebase

--- android/app/src/main/java/com/firealarm/MainApplication.java
+++ android/app/src/main/java/com/firealarm/MainApplication.java
@@ -3,6 +3,8 @@ package com.firealarm;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.database.RNFirebaseDatabasePackage; // <-- Add this line
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,8 +24,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-          ,new VectorIconsPackage()
+          new MainReactPackage(),
+          new RNFirebasePackage(),
+          new RNFirebaseDatabasePackage(), // <-- Add this line
+          new VectorIconsPackage()
       );
     }
 
